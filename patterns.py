29cb93d535e7730d9e95ca25398ad7a9df1dcb25
Ashutosh Raj Singh <ashutosh200210@outlook.com>
2020-04-12 21:45:47 +0530

    added pattern2

--- patterns.py
+++ patterns.py
@@ -23,4 +23,13 @@ def pattern1():
 #7 8 9 10
 #11 12 13 14
 def pattern2():
-    
+    i=1
+    num=1
+    while i<=5:
+        j=1
+        while j<=i:
+            print(num, end=' ')
+            j+=1
+            num+=1
+        print("")
+        i+=1
