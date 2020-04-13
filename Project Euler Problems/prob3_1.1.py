90d28533df5b2e9b13c6aad91f7d6ee70100bb9b
Ashutosh Raj Singh <ashutosh200210@outlook.com>
2020-04-13 18:02:20 +0530

    Added comment documentation

--- Project Euler Problems/prob3_1.1.py
+++ Project Euler Problems/prob3_1.1.py
@@ -1,8 +1,10 @@
+#Program to find largest prime factor of a given number
+
 from sympy.ntheory import factorint
 def largestpf(num):
     largpf=0
-    pflist=list(dict.keys(factorint(num)))
-    for x in pflist:
+    pflist=list(dict.keys(factorint(num))) #read the sympy factorint() documentation for details, basically returns prime factors of a number as keys in a dicitonary
+    for x in pflist:                       #basic loop to determine largest value in a list
         if x>=largpf:
             largpf=x
     return largpf
