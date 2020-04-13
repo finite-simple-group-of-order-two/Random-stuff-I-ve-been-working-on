45972a8b170f4f24b29c65a0274f63303a0d278f
Ashutosh Raj Singh <ashutosh200210@outlook.com>
2020-04-13 18:02:01 +0530

    added comment documentation

--- Project Euler Problems/prob2.py
+++ Project Euler Problems/prob2.py
@@ -2,11 +2,11 @@
 
 def fibonacci_even_sum():
     i=1
-    num=[1,1]
+    num=[1,1]                           #Manually defining first 2 elements of fibonacci series in a list
     sum=0
-    while num[i]<=4000000:
-        num.append(num[i]+num[i-1])
-        if num[i]%2==0:
+    while num[i]<=4000000:              #generating more members of series till they exceed 4 mil
+        num.append(num[i]+num[i-1])     #adding newly generated elements into list
+        if num[i]%2==0:                 #Checking for divisibility by 2
             sum=sum+num[i]
         i+=1
     return(sum)
